e71e38ba285b2c28ff3bb48f8d505d272098c534
Ddbot <rakotoniaina.andry@gmail.com>
2025-12-16 14:14:17 +0100

    Creates basic tech logos grid

--- components/grid-background.tsx
+++ components/grid-background.tsx
@@ -8,21 +8,26 @@ export function GridPatternDemo() {
     <div className="bg-background relative flex h-dvh w-full flex-col items-center justify-center overflow-hidden rounded-lg">
       <GridPattern
         squares={[
-          [10, 17],
-          [11, 17],
-          [12, 17],
-          [13, 17],
-          [14, 17],
-          [15, 17],
-          [16, 17],
-          [17, 17],
-          [18, 17],
-          [19, 17],
-          [20, 17],
-          [21, 17],
-        ]}
+[19+2, 2+9],
+[19+5, 2+9],
+[19+8, 2+9],
+[19+3, 3+9],
+[19+6, 3+9],
+[19+2, 8+9],
+[19+5, 7+9],
+[19+8, 7+9],
+[19+5, 8+9],
+[19+8, 8+9],
+[19+2, 5+9],
+[19+5, 5+9],
+[19+8, 5+9],
+[19+3, 6+9],
+[19+7, 6+9],
+              ]}
+              
+
         className={cn(
-        //   "[mask-image:radial-gradient(400px_circle_at_70%,white,transparent)]",
+          "[mask-image:radial-gradient(400px_circle_at_70%_40%,white,transparent)]",
           "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
         )}
       />
