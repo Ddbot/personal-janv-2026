b0f1bd14d68881f1231ac53c4aa3e8afd3cf1457
Ddbot <rakotoniaina.andry@gmail.com>
2025-12-18 22:57:17 +0100

    Adds Smooth scroll behaviour on root

--- components/grid-background.tsx
+++ components/grid-background.tsx
@@ -6,14 +6,15 @@ import { Button } from "./ui/button"
 import Link from "next/link"
 import { Mail } from 'lucide-react';
 import { TypingAnimation } from "./ui/typing-animation"
+import styles from './grid-background.module.css';
 
 export function GridPatternDemo() {
   return (
     <div className="bg-background relative flex h-dvh w-full overflow-hidden rounded-lg pt-32">
-        <section className="w-1/2 h-full flex flex-col flex-wrap justify-start items-start text-left text-2xl px-8 md:translate-x-8">
+        <section className="w-1/2 h-full flex flex-col flex-wrap justify-start items-start text-left text-2xl px-8">
             <div className="w-full h-fit text-5xl font-light my-2">Développeur <span className="font-bold text-primary">Front End</span></div>
               <div className="w-full h-fit text-5xl font-light my-2">JS <span className="text-primary-foreground">|</span> React <span className="text-primary-foreground">|</span> Next.js</div>
-              <TypingAnimation className="text-5xl my-2" words={[
+              <TypingAnimation className={styles.typing_animation} words={[
                 "Polyvalent", "Curieux", "Fiable","Team Player"
               ]}
                 loop={true}
