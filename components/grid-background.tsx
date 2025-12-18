92137ceada49eb2a00cb54b06b35b14947f1e280
Ddbot <rakotoniaina.andry@gmail.com>
2025-12-18 22:01:28 +0100

    Creates copy for presentation screen

--- components/grid-background.tsx
+++ components/grid-background.tsx
@@ -2,53 +2,72 @@
 
 import { cn } from "@/lib/utils"
 import { GridPattern } from "./ui/grid-pattern"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import { Mail } from 'lucide-react';
 
 export function GridPatternDemo() {
   return (
-      <div className="bg-background relative flex h-dvh w-full flex-col items-left justify-center overflow-hidden rounded-lg">
-          <p className="w-1/2 h-full text-left ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam consectetur ad quos. Eaque, deleniti iure. Reprehenderit facere temporibus nesciunt laborum, aperiam enim accusantium cupiditate sapiente repudiandae earum eos aut quasi?</p>
-      <GridPattern
-              squares={[
+    <div className="bg-background relative flex h-dvh w-full overflow-hidden rounded-lg pt-32">
+        <section className="w-1/2 h-full flex flex-col flex-wrap justify-start items-start text-left text-2xl p-8">
+            <div className="w-full h-fit text-5xl font-bold my-4">JS <span className="text-primary">|</span> React <span className="text-primary">|</span> Next.js</div>
+            <div className="w-full h-fit text-5xl font-bold my-4">Polyvalent, Curieux, Fiable</div>
+            <div className="w-full h-fit text-5xl font-bold my-4">Team Player </div>              
+            <div className="w-full flex flex-row justify-end gap-4 mt-8">
+                <Button variant='outline' className="text-xl p-4">
+                    <Link href="#projects">
+                        Plus d&apos;infos
+                    </Link>
+                </Button>  
+                <Button variant='secondary' className="text-xl p-4">
+                    <Link href="#contact">
+                        Contact
+                    </Link>
+                </Button>  
+            </div>
+        </section>
+        <GridPattern
+            squares={[
             // html
-                  [19, 11],
-                  //   css                  
-                  [23, 2 + 9],
+                [19, 11],
+                //   css                  
+                [23, 2 + 9],
 // tailwind
-                  [27, 2 + 9],
+                [27, 2 + 9],
 // js
-                  [21, 13],
+                [21, 13],
 // ts
-                  [25, 13],
+                [25, 13],
 // react
-                  [29, 13],
+                [29, 13],
 // next
-                  [20, 15],
+                [20, 15],
 // postgres
-                  [24, 15],
+                [24, 15],
 // firebase
-                  [19+6, 8+9],
+                [19+6, 8+9],
 // supabase
-                  [19+8, 8+8],
+                [19+8, 8+8],
 // apps script
-                  [19 + 8, 5 + 9],
+                [19 + 8, 5 + 9],
                 //   figma
-                  [20, 5 + 10],
+                [20, 5 + 10],
                 //   git
-                  [22, 16],
+                [22, 16],
 // openai
-                  [21,18],
+                [21,18],
 // gsap
-                  [28, 18],
+                [28, 18],
                 //   gemini
-                  [19, 17],
-              ]}
-              
+                [19, 17],
+            ]}
+            
 
         className={cn(
-          "[mask-image:radial-gradient(400px_circle_at_70%_40%,white,transparent)]",
-          "inset-x-0 inset-y-[-20%] h-[200%] skew-y-12"
+        "[mask-image:radial-gradient(400px_circle_at_70%_40%,white,transparent)]",
+        "inset-x-0 inset-y-[-20%] h-[200%] skew-y-12"
         )}
-      />
+    />
     </div>
   )
 }
--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-const Logo = ({theme}: {theme: string}) => {
+const Logo = () => {
     return (
         // <Image src={`/logo-${theme}.png`} alt="Logo" width={48} height={48} />
         <h1 className="text-6xl font-extrabold tracking-tight text-primary">andry</h1>
