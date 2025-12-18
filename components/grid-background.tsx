c078cb6e9274711cb55118334dc477edee5b1f5b
Ddbot <rakotoniaina.andry@gmail.com>
2025-12-18 22:31:33 +0100

    Adds words array

--- components/grid-background.tsx
+++ components/grid-background.tsx
@@ -5,14 +5,24 @@ import { GridPattern } from "./ui/grid-pattern"
 import { Button } from "./ui/button"
 import Link from "next/link"
 import { Mail } from 'lucide-react';
+import { TypingAnimation } from "./ui/typing-animation"
 
 export function GridPatternDemo() {
   return (
     <div className="bg-background relative flex h-dvh w-full overflow-hidden rounded-lg pt-32">
         <section className="w-1/2 h-full flex flex-col flex-wrap justify-start items-start text-left text-2xl px-8 md:translate-x-8">
             <div className="w-full h-fit text-5xl font-light my-2">Développeur <span className="font-bold text-primary">Front End</span></div>
-            <div className="w-full h-fit text-5xl font-light my-2">JS <span className="text-primary-foreground">|</span> React <span className="text-primary-foreground">|</span> Next.js</div>
-            <div className="w-full h-fit text-5xl font-light my-2">Polyvalent, Curieux, Fiable</div>
+              <div className="w-full h-fit text-5xl font-light my-2">JS <span className="text-primary-foreground">|</span> React <span className="text-primary-foreground">|</span> Next.js</div>
+              <TypingAnimation className="text-5xl my-2" words={[
+                "Polyvalent", "Curieux", "Fiable","Team Player"
+              ]}
+                loop={true}
+                  delay={1000}
+                  deleteSpeed={100}
+                  cursorStyle="block"
+                  blinkCursor={false}
+              />
+            {/* <div className="w-full h-fit text-5xl font-light my-2">Polyvalent, Curieux, Fiable</div> */}
             {/* <div className="w-full h-fit text-5xl font-bold my-2">Team Player </div>               */}
             <div className="w-full flex flex-row justify-end gap-4 p-8">
                 <Button variant='outline' className="text-sm p-4">
@@ -20,7 +30,8 @@ export function GridPatternDemo() {
                         Plus d&apos;infos
                     </Link>
                 </Button>  
-                <Button variant='secondary' className="text-sm p-4">
+                  <Button variant='secondary' className="text-sm p-4">
+                      <Mail size="icon"/>
                     <Link href="#contact">
                         Contact
                     </Link>
