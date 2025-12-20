80b32d80811dfea46ea23638fb656881c44a0849
Ddbot <rakotoniaina.andry@gmail.com>
2025-12-20 22:30:03 +0100

    Adds decodeHTMLEntities in lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function decodeHtmlEntities(text: string) {
+//   if (typeof document !== 'undefined') {
+//     const textarea = document.createElement('textarea');
+//     textarea.innerHTML = text;
+//     return textarea.value;
+//   }
+  // Server-side fallback
+  return text
+    .replace(/&nbsp;/g, ' ')
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'");
+}
