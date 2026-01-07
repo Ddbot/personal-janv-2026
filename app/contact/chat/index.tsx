5b849b973584497357612a94b94302306ab2b9e6
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-07 22:26:51 +0100

    Sets ut chat header
    base

--- app/contact/chat/index.tsx
+++ app/contact/chat/index.tsx
@@ -1,19 +1,30 @@
 import { Chat } from "@/components/chat/chat";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatHeaderStart, ChatHeaderMain, ChatHeaderEnd } from "@/components/chat/chat-header";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar";
+import { Phone, Video } from 'lucide-react';
 
 export default function ChatPage() {
     return (
         <Chat>
             <ChatHeader>
                 <ChatHeaderStart>
-                    Chat
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>A</AvatarFallback>
+                    </Avatar>
+                    <span className="font-medium">Andry Nirina</span>
                 </ChatHeaderStart>
                 <ChatHeaderMain>
-                    Main
+                    <span className="text-sm font-medium">Développeur Front End</span>
                 </ChatHeaderMain>
-                <ChatHeaderEnd>
-                    End
+                <ChatHeaderEnd className="w-fit p-0">
+                    <Phone width={20} height={20} className="mx-1"/>
+                    <Video width={20} height={20} className="mx-1"/>
                 </ChatHeaderEnd>
             </ChatHeader>
         </Chat>
