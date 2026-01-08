eaa9367261e738954600d9a7a08e488426f90018
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-08 10:04:29 +0100

    Enhances chat header styling and appearance
    Improves the chat header's visual design by applying custom styles for better integration with the application's dark mode and overall theme.
    This change includes adding a style module for the header and updating the header component to use these styles.

--- app/contact/chat/Header.jsx
+++ app/contact/chat/Header.jsx
@@ -1,25 +1,34 @@
 import { ChatHeader, ChatHeaderStart, ChatHeaderMain, ChatHeaderEnd } from "@/components/chat/chat-header";
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { Phone, Video } from 'lucide-react';
+import styles from './styles.module.css';
 const Header = () => {
     return (
-        <ChatHeader className="px-4">
-                <ChatHeaderStart>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback>A</AvatarFallback>
-                    </Avatar>
-                    <span className="font-medium">Andry Nirina</span>
-                </ChatHeaderStart>
-                <ChatHeaderMain>
-                    <span className="text-sm font-medium">Développeur Front End</span>
-                </ChatHeaderMain>
-                <ChatHeaderEnd className="w-fit p-0">
-                    <Phone width={20} height={20} className="mx-2"/>
-                    <Video width={20} height={20} className="mx-2"/>
-                </ChatHeaderEnd>
-</ChatHeader>
-    );
+		<ChatHeader className={styles.header}>
+			<ChatHeaderStart>
+				<Avatar>
+					<AvatarImage
+						src="https://github.com/shadcn.png"
+						alt="@shadcn"
+					/>
+					<AvatarFallback>A</AvatarFallback>
+				</Avatar>
+				<span className="font-medium">Andry Nirina</span>
+				<span className="text-sm font-medium">
+					<b className="text-(--color-20)">/</b>
+				</span>
+			</ChatHeaderStart>
+			<ChatHeaderMain>
+				<span className="text-sm font-medium text-primary">
+					Développeur Front End
+				</span>
+			</ChatHeaderMain>
+			<ChatHeaderEnd className="w-fit p-0">
+				<Phone width={20} height={20} className="mx-2" />
+				<Video width={20} height={20} className="mx-2" />
+			</ChatHeaderEnd>
+		</ChatHeader>
+	);
 };
             
 export default Header;
