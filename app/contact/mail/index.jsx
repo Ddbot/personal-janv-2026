5dc25294d72d02f916e5a41f9104242b4f7a3646
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-08 15:52:41 +0100

    Makes Mail message container scrollable

--- app/contact/mail/index.jsx
+++ app/contact/mail/index.jsx
@@ -1,10 +1,12 @@
 import { Mail } from "lucide-react";
 import { Textarea } from '@/components/ui/textarea';
+import { Button } from "@/components/ui/button";
 import styles from './styles.module.css';
 import { Card, CardContent, CardHeader, CardFooter, CardTitle } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Send, Trash2 } from 'lucide-react';
 
 const MailPage = () => {
     return (
@@ -16,12 +18,22 @@ const MailPage = () => {
 					<Input name="sujet" className="block ml-4 border-0" />
 				</CardTitle>
 			</CardHeader>
-			<Textarea
-				name="body"
-				className={styles.textarea}
-				// field-sizing: Content; + defensive styling
-				placeholder="Write your message here..."
-			/>
+			<CardContent className="relative w-full h-full flex justify-center items-center p-0 m-0">    
+				<Textarea
+					name="body"
+					className={styles.textarea}
+					// field-sizing: Content; + defensive styling
+					placeholder="Write your message here..."
+				/>
+			</CardContent>
+			<CardFooter className="w-full h-fit flex flex-row items-start justify-end gap-2 p-2 pb-3 m-0">
+				<Button variant="outline" className="m-0">
+					<Trash2 />
+				</Button>
+				<Button className="m-0">
+					<Send />
+				</Button>
+			</CardFooter>
 		</Card>
 	);
 }
