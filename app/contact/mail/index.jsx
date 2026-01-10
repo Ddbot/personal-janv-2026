ccccc1bc661a5a048478e04251ca1b6b87d0911f
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-10 20:11:15 +0100

    Deletes useless imports in Mail page

--- app/contact/mail/index.jsx
+++ app/contact/mail/index.jsx
@@ -1,24 +1,14 @@
-import { Mail } from "lucide-react";
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from "@/components/ui/button";
 import styles from './styles.module.css';
 import { Card, CardContent, CardHeader, CardFooter, CardTitle } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
     Send, Trash2,
-	Phone,
-	Video,
-	PlusIcon,
-	Gift,
-	CalendarDays,
-	SquareChevronRight,
 } from 'lucide-react';
 import {
 	ChatToolbar,
-	ChatToolbarAddonStart,
-	ChatToolbarTextarea,
 	ChatToolbarAddonEnd,
 } from '@/components/chat/chat-toolbar';
 
@@ -56,7 +46,7 @@ const MailPage = () => {
 							variant="outline">
 							<Trash2 className="size-4 @md/chat:size-5 stroke-[1.7px]" />
 						</Button>
-						<Button className="size-8 @md/chat:size-9">
+						<Button type="submit" className="size-8 @md/chat:size-9">
 							<Send className="size-4 @md/chat:size-5 stroke-[1.7px]" />
 						</Button>
 					</ChatToolbarAddonEnd>
