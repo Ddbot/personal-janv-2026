d3783b742cacde558b9591a1600e4d0cd6fd6223
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-25 01:37:05 +0100

    Restyles hero section for improved aesthetics
    Updates the hero section's styling by moving the typing animation styles to the hero module.
    This change enhances the visual appeal and maintains consistency in the component.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,13 @@
 "use client"
 import { use, useEffect } from 'react'
-import { cn } from "@/lib/utils"
-import { GridPattern } from "./ui/grid-pattern"
 import { Button } from "./ui/button"
 import Link from "next/link"
-import { Mail, BookText } from 'lucide-react';
+import { Mail } from 'lucide-react';
 import { TypingAnimation } from "./ui/typing-animation"
-import { RainbowButton } from "./ui/rainbow-button"
-import styles from './grid-background.module.css';
+import styles from './styles/hero.module.css';
 import { dictionary } from '../lib/dictionary';
 import { LangContext } from "../contexts/LangContext";
 import LogosGrid from './logos-grid';
-import styles_hero from './styles/hero.module.css';
 
 const CustomTypingAnimation  = (props: React.ComponentPropsWithoutRef<'div'>) => {
     return <TypingAnimation {...props} />
@@ -23,8 +19,8 @@ export default function Hero({ className }: { className: string }) {
         window.scrollTo(0, 0);
     }, []);
     return (
-        <div className={ styles_hero.hero}>
-            <section className={ styles_hero.section}>
+        <div className={ styles.hero}>
+            <section className={ styles.section}>
                 <div className="w-full h-fit text-3xl md:text-5xl font-light my-2">{dictionary[lang]['hero']['intro'][0]}<span className="lg:font-bold text-primary">{dictionary[lang]['hero']['intro'][1]}</span></div>
                 <div className="w-full h-fit text-3xl md:text-5xl font-light my-2">JS <span className="text-primary">|</span> React <span className="text-primary">|</span> Next.js</div>
                 <CustomTypingAnimation className={styles.typing_animation} words={dictionary[lang]['hero']['typing']}
