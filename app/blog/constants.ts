f72805878d8d92b8f309072e39453519d4276ea4
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-26 10:51:26 +0100

    Adds tags list constant
    Defines a constant containing a list of tags for blog posts.
    
    This allows for consistent and easy management of tags
    across the blog application.

---
+++ app/blog/constants.ts
@@ -0,0 +1,43 @@
+type Tag = {
+    name: string;
+}
+
+const tags_list: Record<string, Tag> = {
+    "696297179": {
+        name: "apps_script"
+    },
+ 
+    "169": {
+        name: "css"
+    },
+ 
+    "6504": {
+        name: "dev"
+    },
+ 
+    "647": {
+        name: "html"
+    },
+ 
+    "21291": {
+        name: "js"
+    },
+ 
+    "636056967": {
+        name: "nextjs"
+    },
+ 
+    "96530": {
+        name: "react"
+    },
+ 
+    "711439700": {
+        name: "supabase"
+    },
+ 
+    "33": {
+        name: "wordpress"
+    }
+}
+
+export { tags_list };
