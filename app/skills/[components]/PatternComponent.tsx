c55a903134ddbd780c20546cf558f85d7e460407
Ddbot <rakotoniaina.andry@gmail.com>
2026-01-30 22:36:32 +0100

    Removes SVG rendering
    This commit temporarily disables the rendering of the SVG component.
    
    The change involves a conditional rendering that currently always evaluates to false, effectively removing the SVG element from the output.

--- app/skills/[components]/PatternComponent.tsx
+++ app/skills/[components]/PatternComponent.tsx
@@ -4,7 +4,7 @@ import { useGSAP } from '@gsap/react';
 
 const SvgComponent = ({ className, ...props }: { className: string }, ref: React.Ref<SVGSVGElement>) => {
   return (
-	<svg
+	true ? null : <svg
 		xmlns="http://www.w3.org/2000/svg"
 		// width={800}
 		// height={800}
